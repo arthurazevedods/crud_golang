6f89cfaf9f71d7d430e64e7fc4c79cdd482ad7a7
arthur <arthurazevedods@gmail.com>
2024-12-03 08:33:22 -0300

    struct produtos & range on html

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html")) // retornar todos os templates html
 
 func main() {
@@ -13,5 +20,14 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+
+	//slice de produtos
+	produtos := []Produto{
+		{Nome: "Camisa", Descricao: "Básica", Preco: 29.99, Quantidade: 10},
+		{Nome: "Garrafa térmica", Descricao: "300ml", Preco: 59.99, Quantidade: 2},
+		{"Copo", "Corrida", 224.99, 5},
+		{"Fone", "Headphone", 99.99, 2},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
