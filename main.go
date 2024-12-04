1e2d12656aaa31eb7cebf7225766e161b0e5570f
arthur <arthurazevedods@gmail.com>
2024-12-04 16:43:47 -0300

    conecta com banco

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
+	"os"
 	"text/template"
+
+	_ "github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
 type Produto struct {
@@ -15,8 +22,11 @@ type Produto struct {
 var temp = template.Must(template.ParseGlob("templates/*.html")) // retornar todos os templates html
 
 func main() {
+	db := conectaComBanco()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8080", nil)
+
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -30,4 +40,22 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	temp.ExecuteTemplate(w, "Index", produtos)
+}
+
+func conectaComBanco() *sql.DB {
+	// Obtém as variáveis de ambiente para conexão
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PWD")
+	dbname := "golang_loja" // Nome fixo do banco (pode ser parametrizado)
+	host := "localhost"     // Host fixo (também pode ser parametrizado)
+	sslmode := "disable"    // Normalmente "disable" para desenvolvimento local
+
+	// Cria a string de conexão no formato correto
+	conexao := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=%s", user, dbname, password, host, sslmode)
+
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
