33ddbd5059327bd5929a55bf4adb19d2af4e604b
arthur <arthurazevedods@gmail.com>
2024-12-05 09:39:28 -0300

    connect with db

--- main.go
+++ main.go
@@ -1,33 +1,80 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"html/template"
 	"net/http"
-	"text/template"
+	"os"
+
+	_ "github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+func conectaComBancoDeDados() *sql.DB {
+
+	// Obter as variáveis de ambiente
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PWD")
+	dbname := os.Getenv("POSTGRES_DB")
+	host := os.Getenv("POSTGRES_HOST")
+	sslmode := os.Getenv("POSTGRES_SSLMODE")
+	port := os.Getenv("POSTGRES_PORT")
+
+	// Construir a string de conexão
+	conexao := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=%s port=%s", user, password, dbname, host, sslmode, port)
+
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
 	Quantidade int
 }
 
-var temp = template.Must(template.ParseGlob("templates/*.html")) // retornar todos os templates html
+var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
 	http.HandleFunc("/", index)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
+	db := conectaComBancoDeDados()
 
-	//slice de produtos
-	produtos := []Produto{
-		{Nome: "Camisa", Descricao: "Básica", Preco: 29.99, Quantidade: 10},
-		{Nome: "Garrafa térmica", Descricao: "300ml", Preco: 59.99, Quantidade: 2},
-		{"Copo", "Corrida", 224.99, 5},
-		{"Fone", "Headphone", 99.99, 2},
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
 	}
 
 	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
