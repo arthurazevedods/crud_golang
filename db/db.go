b4855c6b8e0fd17aba731924b4d82a494fc10912
arthur <arthurazevedods@gmail.com>
2024-12-07 16:46:22 -0300

    init modularization

---
+++ db/db.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+func ConectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres port=5433 dbname=golang_loja password=admin2019 host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
--- main.go
+++ main.go
@@ -1,43 +1,12 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"html/template"
 	"net/http"
-	"os"
 
-	_ "github.com/joho/godotenv"
+	"github.com/arthurazevedods/models"
 	_ "github.com/lib/pq"
 )
-
-func conectaComBancoDeDados() *sql.DB {
-
-	// Obter as variáveis de ambiente
-	user := os.Getenv("POSTGRES_USER")
-	password := os.Getenv("POSTGRES_PWD")
-	dbname := os.Getenv("POSTGRES_DB")
-	host := os.Getenv("POSTGRES_HOST")
-	sslmode := os.Getenv("POSTGRES_SSLMODE")
-	port := os.Getenv("POSTGRES_PORT")
-
-	// Construir a string de conexão
-	conexao := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=%s port=%s", user, password, dbname, host, sslmode, port)
-
-	db, err := sql.Open("postgres", conexao)
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
-}
-
-type Produto struct {
-	Id         int
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -47,34 +16,6 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	db := conectaComBancoDeDados()
-
-	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Produto{}
-	produtos := []Produto{}
-
-	for selectDeTodosOsProdutos.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Preco = preco
-		p.Quantidade = quantidade
-
-		produtos = append(produtos, p)
-	}
-
-	temp.ExecuteTemplate(w, "Index", produtos)
-	defer db.Close()
+	todosProdutos := models.BuscaTodosOsProdutos()
+	temp.ExecuteTemplate(w, "Index", todosProdutos)
 }
