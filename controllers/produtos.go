0e50c4f935c0c43b88acfe8c5b0560bfabe6429b
arthur <arthurazevedods@gmail.com>
2024-12-07 17:03:28 -0300

    controllers & new product page

---
+++ controllers/produtos.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/arthurazevedods/models"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	todosProdutos := models.BuscaTodosOsProdutos()
+	temp.ExecuteTemplate(w, "Index", todosProdutos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/arthurazevedods/controllers"
+)
+
+func CarregaRotas() {
+	http.HandleFunc("/", controllers.Index)
+	http.HandleFunc("/new", controllers.New)
+}
